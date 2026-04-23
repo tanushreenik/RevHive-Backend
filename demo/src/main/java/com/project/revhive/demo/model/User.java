e867a67b070cefc06282dc0910040b4dc3bcf408
sourav-kr14 <souravkr93@gmail.com>
2026-04-23 16:01:27 +0530

    update: initialized basic project structure

---
+++ demo/src/main/java/com/project/revhive/demo/model/User.java
@@ -0,0 +1,32 @@
+package com.project.revhive.demo.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+
+}
