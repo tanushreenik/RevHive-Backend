9440e4ddb5c3288e55dc2005587f0e36cf5ed943
sourav-kr14 <souravkr93@gmail.com>
2026-04-27 12:16:17 +0530

    update: er diagram added

--- demo/src/main/java/com/project/revhive/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/project/revhive/demo/config/SecurityConfig.java
@@ -1,5 +1,23 @@
 package com.project.revhive.demo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
 }
