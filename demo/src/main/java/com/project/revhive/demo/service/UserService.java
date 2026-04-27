642f1cbb203789ddf6711864663affd4f50514ba
sourav-kr14 <souravkr93@gmail.com>
2026-04-27 14:50:34 +0530

    update: logger added in User Service

--- demo/src/main/java/com/project/revhive/demo/service/UserService.java
+++ demo/src/main/java/com/project/revhive/demo/service/UserService.java
@@ -5,23 +5,31 @@ import com.project.revhive.demo.enums.Role;
 import com.project.revhive.demo.model.User;
 import com.project.revhive.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private static final Logger logger= (Logger) LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
   public User register(RegisterRequest registerRequest)
   {
+
+      logger.info("Register request received for email: {} ");
       if(userRepository.findByEmail(registerRequest.getEmail()).isPresent())
       {
+          logger.warning("Email already exists");
           throw new RuntimeException("Email already exists");
       }
       if(userRepository.findByUsername(registerRequest.getUsername()).isPresent())
       {
+          logger.warning("Username already exists");
           throw new RuntimeException("User name is already present");
       }
 
@@ -32,6 +40,8 @@ public class UserService {
               .bio(registerRequest.getBio())
 //              .avatarUrl(registerRequest.getAvatarUrl())
               .build();
+
+      logger.info("User successfully registered");
 
       return userRepository.save(user);
 
