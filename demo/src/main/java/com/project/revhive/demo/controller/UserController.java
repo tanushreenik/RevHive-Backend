39d6af89e52c91f4095cab8d1ae43681a95b5ab5
sourav-kr14 <souravkr93@gmail.com>
2026-04-27 14:57:39 +0530

    update: swagger and loggers added in user registration

--- demo/src/main/java/com/project/revhive/demo/controller/UserController.java
+++ demo/src/main/java/com/project/revhive/demo/controller/UserController.java
@@ -2,24 +2,36 @@ package com.project.revhive.demo.controller;
 
 
 import com.project.revhive.demo.dto.request.RegisterRequest;
+import com.project.revhive.demo.model.User;
 import com.project.revhive.demo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("api/auth")
 @RequiredArgsConstructor
+@Tag(name = "AUTH API" , description = "User Authentication options")
 public class UserController {
+    private  static final Logger logger= (Logger) LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
+    @Operation(summary = "End point to register a new user")
     @PostMapping("/register")
     public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request)
     {
-        return ResponseEntity.ok(userService.register((request)));
+        logger.info("Incoming register request with email: "+ request.getEmail());
+        User user= userService.register(request);
+        logger.info("User  registration successfully "+ request.getEmail());
+        return ResponseEntity.ok(user);
     }
 }
